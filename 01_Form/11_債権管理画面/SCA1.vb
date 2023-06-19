e0ea14c7b7f626f52d565fab9dfcf99537a566c3
miso <hiro@ilc-soft.co.jp>
2023-06-19 16:09:19 +0900

    顧客情報に団信加入サインの表示を追加

--- 01_Form/11_債権管理画面/SCA1.vb
+++ 01_Form/11_債権管理画面/SCA1.vb
@@ -9,6 +9,7 @@ Public Class SCA1
     Public CID As String = ""                       ' 顧客番号(機構番号)
     Public addSW                                    ' 追加/編集ボタンの識別子(SCA1_S2で判別)
     Private ReadOnly cmn As New Common
+    Private ReadOnly sccmn As New SCcommon
     Private ReadOnly log As New Log
     Private ReadOnly exc As New CExcel
     Public ReadOnly db As New Sqldb
@@ -490,6 +491,7 @@ Public Class SCA1
                     dgv(1, 5).Value = cInfo.Item(17)                                 ' 勤務先
                     dgv(3, 1).Value = cInfo.Item(13)                                 ' TEL1
                     dgv(3, 2).Value = cInfo.Item(11)                                 ' 生年月日
+                    dgv(3, 3).Value = sccmn.GetGroupCredit(cInfo.Item(20))              ' 団信加入サイン
                     dgv(3, 5).Value = cInfo.Item(18)                                 ' 勤務先TEL1
                     ' 連帯債務者
                     dgv(1, 6).Value = cInfo.Item(30)                                 ' ヨミカナ
@@ -499,6 +501,7 @@ Public Class SCA1
                     dgv(1, 10).Value = cInfo.Item(37)                                ' 勤務先
                     dgv(3, 6).Value = cInfo.Item(33)                                 ' TEL1
                     dgv(3, 7).Value = cInfo.Item(31)                                 ' 生年月日
+                    dgv(3, 8).Value = sccmn.GetGroupCredit(cInfo.Item(40))              ' 団信加入サイン
                     dgv(3, 10).Value = cInfo.Item(38)                                ' 勤務先TEL1
                     ' 証券番号(アシスト)
                     Dim dr As DataRow() = db.OrgDataTable(Sqldb.TID.SCAS).Select(String.Format("C02 = '{0}'", cid))
@@ -1396,12 +1399,12 @@ Public Class SCA1
                     {"債権番号", "", "証券番号", "", "契約種別", ""},
                     {"ﾖﾐｶﾅ", "", "TEL", "", "金消契約日", ""},
                     {"債務者", "", "生年月日", "", "貸付金額", ""},
-                    {"郵便番号", "", "", "", "残高", ""},
+                    {"郵便番号", "", "団信加入", "", "残高", ""},
                     {"住所", "", "", "", "貸付金額(B)", ""},
                     {"勤務先", "", "勤務先TEL", "", "残高(B)", ""},
                     {"ﾖﾐｶﾅ", "", "TEL", "", "残高更新日", ""},
                     {"連債者", "", "生年月日", "", "返済額", ""},
-                    {"郵便番号", "", "", "", "返済額(B)", ""},
+                    {"郵便番号", "", "団信加入", "", "返済額(B)", ""},
                     {"住所", "", "", "", "延滞月数", ""},
                     {"勤務先", "", "勤務先TEL", "", "延滞合計額", ""}
                 }
---
+++ 80_SCcommon/SCcommon.vb
@@ -0,0 +1,16 @@
+﻿Public Class SCcommon
+    ' 団信加入サインステータス
+    Public ReadOnly GROUPCREDIT() As String = {"未加入", "加入(単生)", "加入(連生)", "脱退済"}
+
+    ' 団信加入サインの値取得
+    Public Function GetGroupCredit(status As String) As String
+        Dim cmn As New Common
+        Dim val As Integer = cmn.Int(status)
+
+        ' 値が範囲外のときは空白を返却
+        If val >= GROUPCREDIT.Length Then Return ""
+
+        ' 正常値返却
+        Return GROUPCREDIT(val)
+    End Function
+End Class
